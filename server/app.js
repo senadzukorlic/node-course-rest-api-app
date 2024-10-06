1414134ede4b1f5a86f096b4a4b5538cdfcde415
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-06 19:27:10 +0200

    fetching list of posts

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+
 const feedRoutes = require("./routes/feed")
 
 const app = express()
@@ -8,12 +9,12 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   //postavljanje dozvola o ukidanju cors-8,dozvola klijentu da da postavlja konteknt type i autorizaciju i da salje metode (post,put,patch i delete)
-  res.setHeader("Acces-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader(
     "Acces-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   )
-  res.setHeader("Acces-Control-Allow-Headers", "Content-Type,Authorization")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization")
   next()
 })
 
@@ -22,3 +23,5 @@ app.use("/feed", feedRoutes)
 app.listen(8080, () => {
   console.log("Server is running on port 8080")
 })
+
+const cors = require("cors")
