4679dc1c33d51accea5ad104817ff114af009b8b
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-11 22:33:20 +0200

    static images & error handling

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const cors = require("cors")
-const mysql = require("mysql2")
+const path = require("path")
 const feedRoutes = require("./routes/feed")
 const sequelize = require("./util/database")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("images", express.static(path.join(__dirname, "images")))
 
 app.use((req, res, next) => {
   //postavljanje dozvola o ukidanju cors-8,dozvola klijentu da da postavlja konteknt type i autorizaciju i da salje metode (post,put,patch i delete)
@@ -20,6 +21,12 @@ app.use((req, res, next) => {
 })
 
 app.use("/feed", feedRoutes)
+app.use((error, req, res, next) => {
+  console.log(error)
+  const status = error.statusCode
+  const message = error.message
+  res.status(status).json({ message: message })
+})
 
 sequelize
   .sync()
