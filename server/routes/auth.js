3c1c8698cdabbf0465655453f2532d1ebb7c30ea
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-22 20:56:30 +0200

    added user signup validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,21 @@
-cosnt express = require('express');
+const express = require('express');
+const { body } = require('express-validator');
+const router = express.Router();
+const User = require('../models/user');
+const authController = require('../controllers/auth');
 
-const Router = express.Router();
+router.put('/signup', [
+    body('email').isEmail().withMessage('Please enter a valid email.')
+.custom((value, { req }) => {
+return User.findOne({ email: value }).then(userDoc => {
+if (userDoc) {
+return Promise.reject('E-Mail address already exists!');}
+}
+);
+    }).normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().not().isEmpty()
 
+],authController.signup);
 
-
-module.exports = Router;
+module.exports = router;
