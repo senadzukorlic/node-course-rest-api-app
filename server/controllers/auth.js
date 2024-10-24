36ca7dbc7a1a377b1500422e9038a44f23963dda
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-24 21:44:08 +0200

    created user login logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -74,3 +74,29 @@ exports.signup = async (req, res, next) => {
     next(err)
   }
 }
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  let loadedUser;
+  User.findOne({ where: { email: email } }).then(user =>{
+    if(!user){
+      const error = new Error('A user with this email could not be found.')
+      error.statusCode = 401
+        throw error
+    }
+  loadedUser = user;
+    return bcrypt.compare(password, user.password)
+  }).then(isEqual =>{
+if(!isEqual){
+    const error = new Error('Wrong password!')
+    error.statusCode = 401
+  throw error
+}
+  }).catch(err => {
+    if (!err.statusCode) {
+        err.statusCode = 500
+    }
+    next(err)
+  })
+}
